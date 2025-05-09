65c88c4850a096140e27e063468028792fca12eb
ani <sparkra1n@proton.me>
2025-05-08 19:32:41 -0700

    Update types for maze algo

--- src/Types.ts
+++ src/Types.ts
@@ -13,7 +13,30 @@ export interface GameContext {
 export interface RenderableObject {
   update(context: GameContext): void;
   getMesh(): Three.Object3D;
+
 }
 
 export type Axis = 'x' | 'y' | 'z' | 'w';
 export type Plane = [Axis, Axis];
+
+export type Grid = number[][];
+
+export class Point2 {
+  public x: number;
+  public y: number;
+
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+export class WallSegment {
+  public p1: Point2;
+  public p2: Point2;
+
+  constructor(p1: Point2, p2: Point2) {
+    this.p1 = p1;
+    this.p2 = p2;
+  }
+}
